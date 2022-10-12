0a41b398fad14e51a3e2bae546c35544366a3633
FerCalde <chipneidev@gmail.com>
2022-10-13 00:42:18 +0200

    AudioListener

--- Audio_plantilla_P1/src/AudioBuffer.cpp
+++ Audio_plantilla_P1/src/AudioBuffer.cpp
@@ -1,11 +1,11 @@
 #include "AudioBuffer.h"
 
-#include "AL/alc.h"
-#include "AL/alext.h"
-#include "AL/alut.h"
-#include "AL/efx.h"
-#include "AL/efx-creative.h"
-#include "AL/efx-presets.h"
+//#include "AL/alc.h"
+//#include "AL/alext.h"
+//#include "AL/alut.h"
+//#include "AL/efx.h"
+//#include "AL/efx-creative.h"
+//#include "AL/efx-presets.h"
 
 
 
